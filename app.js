da0583c1baad37109762b923c7d7eb06f9f201fb
Apoorva Teli <apoorvateli@gmail.com>
2017-10-01 03:43:49 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("search");
+});
+
+app.get("/results", function(req, res) {
+  var query = req.query.search;
+  console.log(query);
+  var url = "http://www.omdbapi.com/?apikey=thewdb&type=movie&s=" + query;
+
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      // res.send(data["Search"][0]["Title"]);
+      res.render("results", {data: data});
+    }
+  });
+});
+
+app.get("*", function(req, res) {
+  res.send("Page not found");
+});
+
+app.listen(3000, function() {
+  console.log("Serving movie-search-app on port 3000");
+})
