5ee7e8197531a3127b45c939b931b670e7d83ed0
Apoorva Teli <apoorvateli@gmail.com>
2017-11-04 03:34:24 +0530

    Make UI improvements

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("a.new_window").attr("target", "_blank");
+  var trending = document.getElementById('trending');
+  var cf = document.getElementById('cf');
+  var search-content = document.getElementById('search-content');
+
+  trending.scrollIntoView();
+  cf.scrollIntoView();
+  search-content.scrollIntoView();
+ });
